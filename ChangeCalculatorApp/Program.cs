90e3621f052a29e4558eebd3a3827646ca8a7471
jchinnici <joseph.t.chinnici@gmail.com>
2019-02-06 19:52:11 -0500

    changed name

--- ChangeCalculatorApp/Program.cs
+++ ChangeCalculatorApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChangeCalculatorApp
 {
@@ -10,6 +6,40 @@ namespace ChangeCalculatorApp
     {
         static void Main(string[] args)
         {
+            //test
+
+
+            Decimal Qtr = 0.25m;
+            Decimal Pny = 0.01m;
+            Decimal Nkl = 0.05m;
+            Decimal Dme = 0.10m;
+            Decimal Dlr = 1.00m;
+            int NmbDlr = 0;
+            int NmbQtr = 0;
+            int NmbDme = 0;
+            int NmbNkl = 0;
+            int NmbPny = 0;
+            Console.WriteLine("Enter Payment");
+            Decimal NmbOfPny = 0;
+            var Payment = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Cost");
+            var Cost = Decimal.Parse(Console.ReadLine());
+            var Change = Payment - Cost;
+            Console.WriteLine("Your change is " + Change);
+
+
+
+            while (Change >= Dlr)
+            {
+                NmbDlr++;
+                Change = Change - Dlr;
+            }
+
+
+            Console.WriteLine("Dollars received " + NmbDlr);
+            Console.Read();
         }
     }
+
 }
