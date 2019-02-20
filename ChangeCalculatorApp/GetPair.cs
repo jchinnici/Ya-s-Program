ac82ff2b55e30cc5f7cbf56c1bc5f96700f7bed1
jchinnici <joseph.t.chinnici@gmail.com>
2019-02-19 21:36:23 -0500

    Created class getPair to answer first question

---
+++ ChangeCalculatorApp/GetPair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCalculatorApp
+{
+    public class GetPair
+    {
+    int[] intarray = new int[] { 1, 4, 2, 3, 3, 6, -1, 7, 5, 1 };
+    
+        
+    public GetPair(int[] intarray)
+        {
+            for (var i = 0; i < intarray.Length - 1; i++)
+                if (intarray[i] + intarray[i + 1] == 6)
+                    Console.WriteLine($"{intarray[i]},{intarray[i + 1]}");
+        }
+    
+
+    }
+    
+}
--- ChangeCalculatorApp/Program.cs
+++ ChangeCalculatorApp/Program.cs
@@ -4,28 +4,23 @@ namespace ChangeCalculatorApp
 {
     class Program
     {
-        static int GetMaxSum(int[] arr) {
-            int lastGreatest = 0;
-            for (var i = 0; i < arr.Length; i++)
-            {
-                if (i == arr.Length - 1 && arr[i] > lastGreatest)
-                        return arr[i];
+        static void GetSixSums(int[] arr)
+        {
 
-                var currentSum = arr[i] + arr[i + 1];
-                if (lastGreatest < currentSum)
-                    lastGreatest = currentSum;
-                
-            }
-            return lastGreatest;
+            
+
+
+
         }
 
         static void Main(string[] args)
         {
-            
+            int[] intarray = new int[] { 1, 4, 2, 3, 3, 6, -1, 7, 5, 1 };
+            Console.WriteLine($"{intarray[1]},{intarray[0]}");
+            new GetPair(intarray);
             //inside a function variables are all lower case
             //int array find the biggest pair of numbers in a given sequence
-            int[] intarray = new int[] { 1, 7, -3, 8, 2, 6, -1, 8, -2, 100 };
-            Console.WriteLine(GetMaxSum(intarray));
+
             new FizzBuzz(Console.ReadLine());
 
 
